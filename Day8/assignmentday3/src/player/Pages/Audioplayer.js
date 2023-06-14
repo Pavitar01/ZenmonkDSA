02913bfbbc4d149b5d131589fc356346597f3360
Pavi <mickeynegi924@gmail.com>
2023-06-14 23:24:05 +0530

    last update

--- Day8/assignmentday3/src/player/Pages/Audioplayer.js
+++ Day8/assignmentday3/src/player/Pages/Audioplayer.js
@@ -1,18 +1,42 @@
 import React, { useState } from "react";
 import Top from "../component/Top";
 import Bottom from "../component/Bottom";
+import allsong from "../services/Data";
 import "../css/all.css";
 
 const Audioplayer = () => {
-  const [value, setvalue] = useState("");
-  const playsongs = (e) => {
-    setvalue(e);
+  const [currentSong, setCurrentSong] = useState(allsong[0]);
+
+  const playSong = (song) => {
+    setCurrentSong(song);
   };
-  
+
+  const playNextSong = () => {
+    const currentIndex = allsong.findIndex((song) => song.id === currentSong.id);
+    const nextIndex = (currentIndex + 1) % allsong.length;
+    setCurrentSong(allsong[nextIndex]);
+  };
+
+  const playPreviousSong = () => {
+    const currentIndex = allsong.findIndex((song) => song.id === currentSong.id);
+    const previousIndex = (currentIndex - 1 + allsong.length) % allsong.length;
+    setCurrentSong(allsong[previousIndex]);
+  };
+
+  const playRandomSong = () => {
+    const randomIndex = Math.floor(Math.random() * allsong.length);
+    setCurrentSong(allsong[randomIndex]);
+  };
+
   return (
     <div className="parent">
-      <Top value={value}/>
-      <Bottom playsongs={playsongs}  />
+      <Top
+        currentSong={currentSong}
+        playNextSong={playNextSong}
+        playPreviousSong={playPreviousSong}
+        playRandomSong={playRandomSong}
+      />
+      <Bottom allsongs={allsong} playSong={playSong} />
     </div>
   );
 };
--- Day8/assignmentday3/src/player/component/Top.js
+++ Day8/assignmentday3/src/player/component/Top.js
@@ -1,20 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Middle from "./Middle";
+import allsong from "../services/Data";
 
-const Top = ({ value }) => {
+const Top = ({ currentSong, playNextSong, playPreviousSong, playRandomSong }) => {
+  const [songTitle, setSongTitle] = useState("");
+  const [songArtist, setSongArtist] = useState("");
+  const [songImage, setSongImage] = useState("");
+
+  useEffect(() => {
+    if (currentSong) {
+      setSongTitle(currentSong.title);
+      setSongArtist(currentSong.artist);
+      setSongImage(currentSong.imgurl);
+    }
+  }, [currentSong]);
+
+  const handleNextSong = () => {
+    playNextSong();
+  };
+
+  const handlePreviousSong = () => {
+    playPreviousSong();
+  };
+
+  const handleRandomSong = () => {
+    playRandomSong();
+  };
 
   return (
-    <>
-      <div className="top">
-        <div className="card">
-          <img src={value.imgurl} className="songimg" />
-          <h1 className="songname">
-            {value.title} &nbsp;--<span className="artist">{value.artist}</span>
-          </h1>
-        </div>
-        <Middle value={value}/>
+    <div className="top">
+      <div className="card">
+        <img src={songImage} className="songimg" alt="Song Cover" />
+        <h1 className="songname">
+          {songTitle} &nbsp;--<span className="artist">{songArtist}</span>
+        </h1>
       </div>
-    </>
+      <Middle
+        currentSong={currentSong}
+        playNextSong={handleNextSong}
+        playPreviousSong={handlePreviousSong}
+      />
+      <button onClick={handleRandomSong} className="random">Random</button>
+    </div>
   );
 };
 
