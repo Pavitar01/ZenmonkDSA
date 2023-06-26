4e34d1989741eee2f31216806968e1f8667b59f3
Pavitar Singh <mickeynegi924@gmail.com>
2023-06-26 18:49:03 +0530

    update

--- Day15/firebaselogin/src/component/Home.js
+++ Day15/firebaselogin/src/component/Home.js
@@ -3,8 +3,14 @@ import "../css/chat.css";
 import logo from "../assets/images/logo.png";
 import User from "./User";
 import Details from "./Details";
+import InputEmoji from "react-input-emoji";
 const Home = () => {
   const [toggle, isToggle] = useState(false);
+  const [text, setText] = useState("");
+
+  function handleOnEnter(text) {
+    console.log("enter", text);
+  }
 
   return (
     <div className="chatMain">
@@ -25,7 +31,7 @@ const Home = () => {
           </div>
         </div>
         <div className="right">
-          <div className="top" style={{position:"relative"}}>
+          <div className="top" style={{ position: "relative" }}>
             <button>
               <i class="fa-regular fa-envelope"></i>
             </button>
@@ -38,21 +44,22 @@ const Home = () => {
             </div>
             <button onClick={() => (toggle ? isToggle(false) : isToggle(true))}>
               <i class="fa-solid fa-user"></i>
-             
             </button>
-            {toggle ? <Details/> : <></>}
+            {toggle ? <Details /> : <></>}
           </div>
           <div className="messages"></div>
           <div className="messageFeild">
-            <div className="inputBox">
-              <input type="text" placeholder="Type Something.." />
-              <i class="fa-solid fa-face-smile"></i>
-            </div>
-            <button>
-              
-              <i class="fa-regular fa-copy"></i>
-            </button>
+            <InputEmoji
+              value={text}
+              onChange={setText}
+              cleanOnEnter
+              onEnter={handleOnEnter}
+              placeholder="Type a message"
+            />
           </div>
+          <button>
+            <i class="fa-regular fa-copy"></i>
+          </button>
         </div>
       </div>
     </div>
